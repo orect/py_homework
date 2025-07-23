2e4694ebbca955861edb3b16038a323b427b8bec
orect <iamorest228@gmail.com>
2025-07-23 19:32:02 +0300

    Update home_work_1.py

--- home_work_1.py
+++ home_work_1.py
@@ -1,24 +1,24 @@
-def senior_salary(path):
+def total_salary(path):
     total = 0
     count = 0
 
     with open(path, 'r') as file:
         for line in file:
-            name, role, salary = line.strip().split(',')
-            if role.lower() == 'senior':
-                total += int(salary)
-                count += 1
+            name, salary = line.strip().split(',')
+            
+            total += int(salary)
+            count += 1
 
     if count == 0:
         return (0, 0)
 
-    average_senior = round(total / count, 2)
-    return (total, average_senior)
+    average_salary = round(total / count, 2)
+    return (total, average_salary)
 
 path = 'employees.txt'
 
-result = senior_salary(path)
+result = total_salary(path)
 
 if result:
-    print(f"Загальна зарплата сеньйорів: {result[0]}")
+    print(f"Загальна зарплата: {result[0]}")
     print(f"Середня зарплата сеньйорів: {result[1]}")
